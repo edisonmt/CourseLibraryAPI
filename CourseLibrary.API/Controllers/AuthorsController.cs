280e6fc78c4631400f8f8dfbf2810dc681e2f88e
Edison Tarel <etarel@blastsia.com>
2020-03-24 11:54:03 +0800

    Created AuthorsController
    Added GET method for authors

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        public readonly ICourseLibraryRepository _courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return new JsonResult(authorsFromRepo);
+        }
+    }
+}
