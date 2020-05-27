99d0ccf62e377f8c7a51557bb9b8015df98ed71b
Edison Tarel <etarel@blastsia.com>
2020-05-27 16:57:08 +0800

    Added Validation of Data and Reporting validation errors

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using AutoMapper;
 using System;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CourseLibrary.API
 {
@@ -29,7 +31,53 @@ namespace CourseLibrary.API
             services.AddControllers(setupAction =>
             {
                 setupAction.ReturnHttpNotAcceptable = true;
-            }).AddXmlDataContractSerializerFormatters();
+            }).AddXmlDataContractSerializerFormatters()
+            .ConfigureApiBehaviorOptions(setupAction =>
+            {
+                setupAction.InvalidModelStateResponseFactory = context =>
+                {
+                    //create a problem details object
+                    var problemDetailsFactory = context.HttpContext.RequestServices
+                    .GetRequiredService<ProblemDetailsFactory>();
+
+                    var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                        context.HttpContext,
+                        context.ModelState);
+
+                    //add additional info not added by default
+                    problemDetails.Detail = "See the errors field for details.";
+                    problemDetails.Instance = context.HttpContext.Request.Path;
+
+                    //find out which status code to use
+                    var actionExecutingContext = context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+                    //if there are modelStare errors & all the arguments were correctly
+                    //found/parsed we're dealing with validation errors
+                    if ((context.ModelState.ErrorCount > 0) &&
+                        (actionExecutingContext?.ActionArguments.Count ==
+                        context.ActionDescriptor.Parameters.Count))
+                    {
+                        problemDetails.Type = "urlFor validation problems";
+                        problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                        problemDetails.Title = "One or more validation errors occured.";
+
+                        return new UnprocessableEntityObjectResult(problemDetails)
+                        {
+                            ContentTypes = { "application/problem+json" }
+                        };
+                    }
+
+                    //if one of the argument wasn't correctly found/ couldn't parsed
+                    //we're dealing with null/unparsable input
+                    problemDetails.Status = StatusCodes.Status400BadRequest;
+                    problemDetails.Title = "One or more errors on input occured.";
+                    return new BadRequestObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json" }
+                    };
+
+                };
+            });
              
             services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
 
---
+++ CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentFromDescriptionAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using CourseLibrary.API.Models;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    public class CourseTitleMustBeDifferentFromDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var course = (CourseForCreationDto)validationContext.ObjectInstance;
+
+            if (course.Title == course.Description)
+            {
+                return new ValidationResult(ErrorMessage,
+                    //"The  provided description should  be different from the title.",
+                    new[] { "CourseForCreationDto" });
+            }
+
+            return ValidationResult.Success;
+
+        }
+    }
+}
